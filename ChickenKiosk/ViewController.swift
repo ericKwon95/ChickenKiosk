c4c5fc22e5c88edd2389044a238540cfd4033eaf
Eden <you8inpark@gmail.com>
2024-11-25 20:59:32 +0900

    feat: 장바구니 컨테이너 View 기본 설정

--- ChickenKiosk/ViewController.swift
+++ ChickenKiosk/ViewController.swift
@@ -6,15 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
     let cartContainerView = UIView()
     let cartTableView = UITableView()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpCartContainerView()
     }
     
-    
+    func setUpCartContainerView() {
+        view.addSubview(cartContainerView)
+        cartContainerView.backgroundColor = .white
+        cartContainerView.layer.cornerRadius = 10
+        cartContainerView.layer.borderWidth = 1
+        cartContainerView.layer.borderColor = UIColor.lightGray.cgColor
+        
+        cartContainerView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(150)
+            make.height.equalTo(200)
+        }
+    }
 }
