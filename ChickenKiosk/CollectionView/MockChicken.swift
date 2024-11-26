11e829df958a417c4b3c39873d7c69362abc039e
emilyj4482 <emilyj4482@gmail.com>
2024-11-26 13:06:49 +0900

    feat: mock data 추가

--- ChickenKiosk/CollectionView/MockChicken.swift
+++ ChickenKiosk/CollectionView/MockChicken.swift
@@ -21,8 +21,8 @@ struct MockChicken {
     }
     
     static let honeySeries: [MockChicken] = [
-        .init(name: "허니오리지날", price: 19000, imageURL: "honeyCombo"),
+        .init(name: "허니오리지날", price: 19000, imageURL: "honeyOriginal"),
         .init(name: "허니콤보", price: 23000, imageURL: "honeyCombo"),
-        .init(name: "허니순살", price: 23000, imageURL: "honeyCombo")
+        .init(name: "허니순살", price: 23000, imageURL: "honeyBoneless")
     ]
 }
