ac5e09d9a0dea41afc0691a5ee6774f6f4ea2aa9
ericKwon95 <ericyong95@gmail.com>
2024-11-26 17:14:51 +0900

    feat: 구현한 버튼 메인 뷰 컨트롤러에 추가

--- ChickenKiosk/ViewController.swift
+++ ChickenKiosk/ViewController.swift
@@ -6,13 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    private let footerView = FooterView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureFooterViewUI()
     }
-
+    
+    /// 하단 뷰 subview에 추가 및 위치 설정
+    func configureFooterViewUI() {
+        view.addSubview(footerView)
+        footerView.translatesAutoresizingMaskIntoConstraints = false
+        footerView.snp.makeConstraints { make in
+            make.leading.equalTo(view.snp.leading).offset(16)
+            make.trailing.equalTo(view.snp.trailing).offset(-16)
+            make.bottom.equalTo(view.snp.bottom).offset(-16)
+            make.height.equalTo(44)
+        }
+    }
+}
+    
 #if DEBUG
 import SwiftUI
 struct PreView: PreviewProvider {
