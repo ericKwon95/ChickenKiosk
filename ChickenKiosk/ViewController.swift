fd875004b7454ad47fc9bab3193299ab60933968
Eden <you8inpark@gmail.com>
2024-11-26 10:41:57 +0900

    feat: 장바구니 Label 추가

--- ChickenKiosk/ViewController.swift
+++ ChickenKiosk/ViewController.swift
@@ -26,6 +26,16 @@ class ViewController: UIViewController {
         cartContainerView.layer.borderWidth = 1
         cartContainerView.layer.borderColor = UIColor.lightGray.cgColor
         
+        let cartLabel = UILabel()
+        cartLabel.text = "장바구니"
+        cartLabel.textAlignment = .left
+        cartLabel.textColor = .black
+        cartContainerView.addSubview(cartLabel)
+        
+        cartLabel.snp.makeConstraints{ make in
+            make.left.equalToSuperview()
+        }
+        
         cartContainerView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(16)
             make.bottom.equalToSuperview().inset(150)
