6340ac60f435dd3cf471cbc25a48174560dc2c3a
ericKwon95 <ericyong95@gmail.com>
2024-11-26 10:56:08 +0900

    feat: 타이틀 뷰 메인 뷰컨트롤러에 적용

--- ChickenKiosk/ViewController.swift
+++ ChickenKiosk/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let titleView = TitleView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
     }
+    
+    private func configureUI() {
+        view.addSubview(titleView)
+        
+        titleView.snp.makeConstraints { make in
+            make.leading.equalTo(view.snp.leading).offset(16)
+            make.trailing.equalTo(view.snp.trailing).offset(-16)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.height.equalTo(50)
+        }
+    }
+}
 
 
 }
