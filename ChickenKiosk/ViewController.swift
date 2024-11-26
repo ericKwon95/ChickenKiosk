6c1b14cfd8bdb85bdee4666d0288f2b33c2624b2
Eden <you8inpark@gmail.com>
2024-11-26 11:33:38 +0900

    docs: 모든 코드의 역할을 부여하는 주석 추가

--- ChickenKiosk/ViewController.swift
+++ ChickenKiosk/ViewController.swift
@@ -10,28 +10,35 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+    // cart 관련 뷰 선언
     let cartContainerView = UIView()
     let cartTableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // cart 컨테이너 뷰 설정 호출
         setUpCartContainerView()
     }
     
+    
+    // cart 컨테이너 뷰 설정 함수
     func setUpCartContainerView() {
+        // 컨테이너 뷰 추가 및 기본 설정
         view.addSubview(cartContainerView)
         cartContainerView.backgroundColor = .white
         cartContainerView.layer.cornerRadius = 10
         cartContainerView.layer.borderWidth = 1
         cartContainerView.layer.borderColor = UIColor.lightGray.cgColor
         
+        // SnapKit 사용해 레이아웃 임의로 설정
         cartContainerView.snp.makeConstraints { make in
             make.leading.trailing.equalToSuperview().inset(16)
             make.bottom.equalToSuperview().inset(150)
             make.height.equalTo(200)
         }
         
+        // 장바구니 label
         let cartLabel = UILabel()
         cartLabel.text = "장바구니"
         cartLabel.textAlignment = .left
