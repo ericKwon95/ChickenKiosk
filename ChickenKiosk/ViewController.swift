d030c2a8c62a817b0063d2f55dd4538c6c452966
“hyunwook-sevo” <seojy1276@naver.com>
2024-11-26 21:07:09 +0900

    perf: divider 완벽 제거

--- ChickenKiosk/ViewController.swift
+++ ChickenKiosk/ViewController.swift
@@ -17,8 +17,13 @@ class ViewController: UIViewController {
         let segmentedControl = UISegmentedControl(items: ["허니시리즈", "레드시리즈", "교촌시리즈"])
         segmentedControl.selectedSegmentIndex = 0 // 기본 선택된 세그먼트
         segmentedControl.backgroundColor = .clear // 배경색 설정
-        segmentedControl.selectedSegmentTintColor = .systemYellow // 선택 색상 설정
+        segmentedControl.selectedSegmentTintColor = .yellow // 선택 색상 설정
         segmentedControl.addTarget(self, action: #selector(segmentChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default) // 배경 제거
+        segmentedControl.setBackgroundImage(UIImage(), for: .selected, barMetrics: .default) // 선택된 상태 배경 제거
+        segmentedControl.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        // Divider 제거
         
         // Divider 제거 및 텍스트 스타일 변경
         let titleAttributes: [NSAttributedString.Key: Any] = [
