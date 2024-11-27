10492d08c4acdeea9539266ca5eaf862bd29364f
t2023-m0019 <t2023-m0019@t2023-m0019ui-MacBookAir.local>
2024-11-27 15:27:16 +0900

    feat: UIViewPreview 기능 추가

---
+++ ChickenKiosk/Extension/UIView+Preview.swift
@@ -0,0 +1,30 @@
+//
+//  UIView+Preview.swift
+//  ChickenKiosk
+//
+//  Created by t2023-m0019 on 11/27/24.
+//
+
+import SwiftUI
+
+// iOS 16.0 이하에서도 UIView를 Preview 사용 가능하도록 하는 익스텐션
+#if DEBUG
+import SwiftUI
+
+extension UIView {
+    private struct Preview: UIViewRepresentable {
+        let view: UIView
+        
+        func makeUIView(context: Context) -> UIView {
+            return view
+        }
+        
+        func updateUIView(_ uiView: UIView, context: Context) {
+        }
+    }
+    
+    func toPreview() -> some View {
+        Preview(view: self)
+    }
+}
+#endif
