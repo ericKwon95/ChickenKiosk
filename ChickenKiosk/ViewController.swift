27a0fce7623684b30884b0b0e630ee563b63fbec
Eric Kwon / 권승용 <ericyong95@gmail.com>
2024-11-27 11:50:11 +0900

    feat: 타이틀 구현 (#3)
    * chroe: 타이틀 에셋 추가
    
    - 귀여운 치킨 아이콘 추가
    
    * feat: 타이틀 뷰 구현
    
    - 이미지 뷰 / 라벨 뷰를 사용해 구현
    
    * feat: 타이틀 뷰 메인 뷰컨트롤러에 적용
    
    * feat: 프리뷰 기능 추가
    
    - iOS 17 이전에서도 사용할 수 있게 extension 설정
    
    * fix: 띄어쓰기 제거
    
    * docs: 주석 작성
    
    * chore: 불필요한 코드 제거
    
    - 스냅킷에서 처리해주는 코드 제거

--- ChickenKiosk/ViewController.swift
+++ ChickenKiosk/ViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    
+    private let titleView = TitleView()
+
     private lazy var collectionView: ChickenCollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -18,13 +19,25 @@ class ViewController: UIViewController {
         let collectionView = ChickenCollectionView(frame: .zero, collectionViewLayout: layout)
         return collectionView
     }()
-    
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
         setCollectionView()
     }
     
-    private func setCollectionView() {
+    private func configureUI() {
+        view.addSubview(titleView)
+        
+        titleView.snp.makeConstraints { make in
+            make.leading.equalTo(view.snp.leading).offset(16)
+            make.trailing.equalTo(view.snp.trailing).offset(-16)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.height.equalTo(50)
+        }
+    }
+  
+   private func setCollectionView() {
         view.addSubview(collectionView)
         
         collectionView.snp.makeConstraints {
@@ -35,3 +48,12 @@ class ViewController: UIViewController {
         }
     }
 }
+
+#if DEBUG
+import SwiftUI
+struct PreView: PreviewProvider {
+    static var previews: some View {
+        ViewController().toPreview()
+    }
+}
+#endif
